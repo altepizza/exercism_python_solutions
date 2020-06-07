57ce939fad23fd533c0266e81b8876e0a29e1541
Kai <git@altepizza.de>
2020-06-07 15:00:57 +0200

    Make grade_school solution python3 like

--- grade-school/grade_school.py
+++ grade-school/grade_school.py
@@ -1,26 +1,24 @@
 class School:
     def __init__(self):
-        self.students = []
+        self.students = {}
 
     def add_student(self, name, grade):
-        self.students.append({name: grade})
+        self.students[name] = grade
 
     def roster(self):
         grades_to_students = {}
         roster = []
-        for student in self.students:
-            name, grade = student.items()[0]
+        for name, grade in self.students.items():
             if grade in grades_to_students:
                 grades_to_students[grade].append(name)
             else:
                 grades_to_students[grade] = [name]
         for key in sorted(grades_to_students.keys()):
-            roster = roster + sorted(grades_to_students[key])
+            roster += sorted(grades_to_students[key])
         return roster
 
     def grade(self, grade_number):
         return sorted([
-            student.keys()[0] for student in self.students 
-            if student[student.keys()[0]] == grade_number
+            student for student in self.students.keys()
+            if self.students[student] == grade_number
         ])
- 
