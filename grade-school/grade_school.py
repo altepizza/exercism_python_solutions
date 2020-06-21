40acfdce1fe7352899a7be52e3a37b96c3b5836f
Kai <git@altepizza.de>
2020-06-21 20:27:51 +0200

    Refactor grade_school

--- grade-school/grade_school.py
+++ grade-school/grade_school.py
@@ -2,22 +2,19 @@ class School:
 
     def __init__(self):
         self.students = {}
-        self.students_sorted = []
 
     def add_student(self, name, grade):
         self.students[name] = grade
-        self.students_sorted = sorted(
+        self.students = dict(sorted(
             self.students.items(), key=lambda student_grade:
                 str(student_grade[1]) + student_grade[0]
-        )
+        ))
 
     def roster(self):
-        return [student[0] for student in self.students_sorted]
+        return [student for student in self.students]
 
     def grade(self, grade_number):
         return [
-            student[0]
-            for student
-            in self.students_sorted
-            if student[1] == grade_number
+            student for student in self.students
+            if self.students[student] == grade_number
         ]
