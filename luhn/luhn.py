953a4e5f85fe4b7c462fcd0f5da78c300228a9d8
Kai <git@altepizza.de>
2020-06-27 15:15:36 +0200

    Tidy up luhn

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -1,26 +1,30 @@
 class Luhn:
-    def __init__(self, card_num):
-        self.is_valid = False
-        card_num = card_num.replace(' ', '')
 
+    def _check_card_num(self, card_num):
+        self.is_valid = False
+
+        card_num = card_num.replace(' ', '')
         if len(card_num) > 1:
             try:
                 digits = [int(digit) for digit in card_num]
-                double_me = False
-                for index, num in reversed(list(enumerate(digits))):
-                    if double_me:
-                        print(index, num, digits[index])
-                        if digits[index] * 2 > 9:
-                            digits[index] = digits[index] * 2 - 9
-                        else:
-                            digits[index] = digits[index] * 2
-                        double_me = False
-                    else:
-                        double_me = True
-                if sum(digits) % 10 == 0:
-                    self.is_valid = True
             except ValueError:
-                pass
+                return False
+            double_me = False
+            for index, num in reversed(list(enumerate(digits))):
+                if double_me:
+                    if num * 2 > 9:
+                        digits[index] = num * 2 - 9
+                    else:
+                        digits[index] = num * 2
+                    double_me = False
+                else:
+                    double_me = True
+            if sum(digits) % 10 == 0:
+                return True
+        return False
+
+    def __init__(self, card_num):
+        self.is_valid = self._check_card_num(card_num)
 
     def valid(self):
         return self.is_valid
