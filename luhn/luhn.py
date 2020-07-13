e8d9f5deea1abfc5bee0f9769e9869f00e71f7ed
Kai <git@altepizza.de>
2020-07-13 19:21:55 +0200

    Reorder and simplify solution for luhn

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -1,30 +1,26 @@
 class Luhn:
 
-    def _check_card_num(self, card_num):
-        self.is_valid = False
+    def __init__(self, card_num):
+        self.is_valid = Luhn._check_card_num(card_num)
 
+    def valid(self):
+        return self.is_valid
+
+    @staticmethod
+    def _check_card_num(card_num):
         card_num = card_num.replace(' ', '')
         if len(card_num) > 1:
             try:
                 digits = [int(digit) for digit in card_num]
             except ValueError:
                 return False
-            double_me = False
-            for index, number in reversed(list(enumerate(digits))):
-                if double_me:
+            digits.reverse()
+            for index, number in enumerate(digits):
+                if index % 2 > 0:
                     if number * 2 > 9:
                         digits[index] = number * 2 - 9
                     else:
                         digits[index] = number * 2
-                    double_me = False
-                else:
-                    double_me = True
             if sum(digits) % 10 == 0:
                 return True
         return False
-
-    def __init__(self, card_num):
-        self.is_valid = self._check_card_num(card_num)
-
-    def valid(self):
-        return self.is_valid
