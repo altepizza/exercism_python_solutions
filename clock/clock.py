f125a4d81306d4127a167db56bc1e221ec005204
Kai <git@altepizza.de>
2020-07-24 21:24:28 +0200

    Another clock solution

--- clock/clock.py
+++ clock/clock.py
@@ -1,23 +1,21 @@
-from datetime import datetime
-from datetime import timedelta
-
-
 class Clock:
     def __init__(self, hour, minute):
-        self.my_clock = datetime.strptime('00', '%H')
-        self.my_clock += timedelta(minutes=minute)
-        self.my_clock += timedelta(hours=hour)
+        self.hour = hour
+        self.minute = 0
+        self.__add__(minute)
 
     def __repr__(self):
-        return self.my_clock.strftime('%H:%M')
+        return '{:02d}:{:02d}'.format(self.hour, self.minute)
 
     def __eq__(self, other):
         return self.__repr__() == other
 
     def __add__(self, minutes):
-        self.my_clock += timedelta(minutes=minutes)
+        hours, self.minute = divmod((self.minute + minutes), 60)
+        self.hour = divmod((self.hour + hours), 24)[1]
         return self
 
     def __sub__(self, minutes):
-        self.my_clock -= timedelta(minutes=minutes)
+        hours, self.minute = divmod((self.minute - minutes), 60)
+        self.hour = divmod((self.hour + hours), 24)[1]
         return self
