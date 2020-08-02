e87923d2ff8872816cd219d379890769a039c213
Kai <git@altepizza.de>
2020-08-02 21:35:26 +0200

    Rename functions

--- markdown/markdown.py
+++ markdown/markdown.py
@@ -13,14 +13,14 @@ def parse(markdown):
             if not in_list:
                 in_list = True
                 current_text = emphasis.group(1)
-                current_text = match_boldness(current_text)
-                current_text = match_italicness(current_text)
+                current_text = match_and_create_bold(current_text)
+                current_text = match_and_create_italic(current_text)
                 line = '<ul><li>' + current_text + '</li>'
             else:
                 current_text = emphasis.group(1)
-                current_text = match_boldness(current_text)
-                current_text = match_italicness(current_text)
-                line = '<li>' + current_text + '</li>'
+                current_text = match_and_create_bold(current_text)
+                current_text = match_and_create_italic(current_text)
+                line = create_lineitem(current_text)
         else:
             if in_list:
                 in_list_append = True
@@ -29,8 +29,8 @@ def parse(markdown):
         emphasis = re.match('<h|<ul|<p|<li', line)
         if not emphasis:
             line = '<p>' + line + '</p>'
-        line = match_boldness(line)
-        line = match_italicness(line)
+        line = match_and_create_bold(line)
+        line = match_and_create_italic(line)
         if in_list_append:
             line = '</ul>' + line
             in_list_append = False
@@ -40,14 +40,14 @@ def parse(markdown):
     return result
 
 
-def match_boldness(text):
+def match_and_create_bold(text):
     match = re.match('(.*)__(.*)__(.*)', text)
     if match:
         return match.group(1) + '<strong>' + match.group(2) + '</strong>' + match.group(3)
     return text
 
 
-def match_italicness(text):
+def match_and_create_italic(text):
     match = re.match('(.*)_(.*)_(.*)', text)
     if match:
         return match.group(1) + '<em>' + match.group(2) + '</em>' + match.group(3)
@@ -62,3 +62,7 @@ def match_and_create_headline(line):
     elif re.match('# (.*)', line):
         line = '<h1>' + line[2:] + '</h1>'
     return line
+
+
+def create_lineitem(text):
+    return '<li>' + text + '</li>'
