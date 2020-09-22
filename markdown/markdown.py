e6b6f5c89e12b9e323c5eecda14c4747f099dcaf
Kai <git@altepizza.de>
2020-09-22 21:04:55 +0200

    More Refactoring

--- markdown/markdown.py
+++ markdown/markdown.py
@@ -1,25 +1,33 @@
 import re
 
+BOLD_RE = re.compile(r'__(.*)__', re.M)
+LIST_RE = re.compile(r'\* (.*)')
+H1_RE = re.compile(r'^# (.*)', re.M)
+H2_RE = re.compile(r'^## (.*)', re.M)
+H6_RE = re.compile(r'^###### (.*)', re.M)
+ITALIC_RE = re.compile(r'_(.*)_', re.M)
+
 
 def parse(markdown):
+    markdown = BOLD_RE.sub(r'<strong>\1</strong>', markdown)
+    markdown = ITALIC_RE.sub(r'<em>\1</em>', markdown)
+    markdown = H1_RE.sub(r'<h1>\1</h1>', markdown)
+    markdown = H2_RE.sub(r'<h2>\1</h2>', markdown)
+    markdown = H6_RE.sub(r'<h6>\1</h6>', markdown)
+
     lines = markdown.split('\n')
     result = ''
     in_list = False
     in_list_append = False
     for line in lines:
-        line = match_and_create_headline(line)
-        emphasis = re.match(r'\* (.*)', line)
+        emphasis = LIST_RE.match(line)
         if emphasis:
             if not in_list:
                 in_list = True
                 current_text = emphasis.group(1)
-                current_text = match_and_create_bold(current_text)
-                current_text = match_and_create_italic(current_text)
                 line = '<ul><li>' + current_text + '</li>'
             else:
                 current_text = emphasis.group(1)
-                current_text = match_and_create_bold(current_text)
-                current_text = match_and_create_italic(current_text)
                 line = create_lineitem(current_text)
         else:
             if in_list:
@@ -29,8 +37,6 @@ def parse(markdown):
         emphasis = re.match('<h|<ul|<p|<li', line)
         if not emphasis:
             line = '<p>' + line + '</p>'
-        line = match_and_create_bold(line)
-        line = match_and_create_italic(line)
         if in_list_append:
             line = '</ul>' + line
             in_list_append = False
@@ -38,30 +44,6 @@ def parse(markdown):
     if in_list:
         result += '</ul>'
     return result
-
-
-def match_and_create_bold(text):
-    match = re.match('(.*)__(.*)__(.*)', text)
-    if match:
-        return match.group(1) + '<strong>' + match.group(2) + '</strong>' + match.group(3)
-    return text
-
-
-def match_and_create_italic(text):
-    match = re.match('(.*)_(.*)_(.*)', text)
-    if match:
-        return match.group(1) + '<em>' + match.group(2) + '</em>' + match.group(3)
-    return text
-
-
-def match_and_create_headline(line):
-    if re.match('###### (.*)', line):
-        line = '<h6>' + line[7:] + '</h6>'
-    elif re.match('## (.*)', line):
-        line = '<h2>' + line[3:] + '</h2>'
-    elif re.match('# (.*)', line):
-        line = '<h1>' + line[2:] + '</h1>'
-    return line
 
 
 def create_lineitem(text):
