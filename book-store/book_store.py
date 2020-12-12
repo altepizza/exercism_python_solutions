efc7058673d830ecfd00245f1846afa5825755c1
Kai <git@altepizza.de>
2020-12-12 21:58:14 +0100

    Nearly done

--- book-store/book_store.py
+++ book-store/book_store.py
@@ -1,8 +1,7 @@
-from copy import deepcopy
+from copy import deepcopy, copy
 from collections import Counter
 
 BOOK_PRICE = 800
-
 SET_OF_2_PRICE = BOOK_PRICE * 0.95 * 2
 SET_OF_3_PRICE = BOOK_PRICE * 0.9 * 3
 SET_OF_4_PRICE = BOOK_PRICE * 0.8 * 4
@@ -16,36 +15,35 @@ def reduce_basket(basket, no_to_be_removed):
     return basket
 
 
-def get_combinations(basket, current_price, prices):
+def backtrack_best_price(basket, current_price, prices):
     current_basket = deepcopy(basket)
     local_prices = deepcopy(prices)
-    unique_books = set(current_basket)
-    len_unique_books = len(unique_books)
-    if unique_books:
-        if len_unique_books == 5:
+    len_unique_books = len(set(current_basket))
+    if len_unique_books:
+        if len_unique_books >= 5:
             tmp_basket = deepcopy(current_basket)
             tmp_basket = reduce_basket(tmp_basket, 5)
-            local_prices.append(get_combinations(
+            local_prices.append(backtrack_best_price(
                 tmp_basket, current_price, local_prices) + current_price + SET_OF_5_PRICE)
         if len_unique_books >= 4:
             tmp_basket = deepcopy(current_basket)
             tmp_basket = reduce_basket(tmp_basket, 4)
-            local_prices.append(get_combinations(
+            local_prices.append(backtrack_best_price(
                 tmp_basket, current_price, local_prices) + current_price + SET_OF_4_PRICE)
         if len_unique_books >= 3:
             tmp_basket = deepcopy(current_basket)
             tmp_basket = reduce_basket(tmp_basket, 3)
-            local_prices.append(get_combinations(
+            local_prices.append(backtrack_best_price(
                 tmp_basket, current_price, local_prices) + current_price + SET_OF_3_PRICE)
         if len_unique_books >= 2:
             tmp_basket = deepcopy(current_basket)
             tmp_basket = reduce_basket(tmp_basket, 2)
-            local_prices.append(get_combinations(
+            local_prices.append(backtrack_best_price(
                 tmp_basket, current_price, local_prices) + current_price + SET_OF_2_PRICE)
         if len_unique_books == 1:
             tmp_basket = deepcopy(current_basket)
             tmp_basket = reduce_basket(tmp_basket, 1)
-            local_prices.append(get_combinations(
+            local_prices.append(backtrack_best_price(
                 tmp_basket, current_price, local_prices) + current_price + BOOK_PRICE)
     else:
         return 0
@@ -53,6 +51,5 @@ def get_combinations(basket, current_price, prices):
 
 
 def total(basket):
-    a = get_combinations(basket, 0, [])
-    print(a)
-    return a
+    basket = sorted(basket)
+    return backtrack_best_price(basket, 0, [])
