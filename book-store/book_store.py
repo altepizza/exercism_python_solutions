9a273eb05d161a9d77729c13a3a00e3736e35e71
Kai <git@altepizza.de>
2020-12-31 19:20:38 +0100

    WIP current state of the bookstore solution

--- book-store/book_store.py
+++ book-store/book_store.py
@@ -1,5 +1,6 @@
-from copy import deepcopy, copy
 from collections import Counter
+from copy import copy
+
 
 BOOK_PRICE = 800
 SET_OF_2_PRICE = BOOK_PRICE * 0.95 * 2
@@ -16,32 +17,32 @@ def reduce_basket(basket, no_to_be_removed):
 
 
 def backtrack_best_price(basket, current_price, prices):
-    current_basket = deepcopy(basket)
-    local_prices = deepcopy(prices)
+    current_basket = basket
+    local_prices = copy(prices)
     len_unique_books = len(set(current_basket))
     if len_unique_books:
         if len_unique_books >= 5:
-            tmp_basket = deepcopy(current_basket)
+            tmp_basket = copy(current_basket)
             tmp_basket = reduce_basket(tmp_basket, 5)
             local_prices.append(backtrack_best_price(
                 tmp_basket, current_price, local_prices) + current_price + SET_OF_5_PRICE)
         if len_unique_books >= 4:
-            tmp_basket = deepcopy(current_basket)
+            tmp_basket = copy(current_basket)
             tmp_basket = reduce_basket(tmp_basket, 4)
             local_prices.append(backtrack_best_price(
                 tmp_basket, current_price, local_prices) + current_price + SET_OF_4_PRICE)
         if len_unique_books >= 3:
-            tmp_basket = deepcopy(current_basket)
+            tmp_basket = copy(current_basket)
             tmp_basket = reduce_basket(tmp_basket, 3)
             local_prices.append(backtrack_best_price(
                 tmp_basket, current_price, local_prices) + current_price + SET_OF_3_PRICE)
         if len_unique_books >= 2:
-            tmp_basket = deepcopy(current_basket)
+            tmp_basket = copy(current_basket)
             tmp_basket = reduce_basket(tmp_basket, 2)
             local_prices.append(backtrack_best_price(
                 tmp_basket, current_price, local_prices) + current_price + SET_OF_2_PRICE)
         if len_unique_books == 1:
-            tmp_basket = deepcopy(current_basket)
+            tmp_basket = copy(current_basket)
             tmp_basket = reduce_basket(tmp_basket, 1)
             local_prices.append(backtrack_best_price(
                 tmp_basket, current_price, local_prices) + current_price + BOOK_PRICE)
