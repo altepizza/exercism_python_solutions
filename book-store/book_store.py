ab0e888c3998d1cdc177fb6bd878f0892fb425f9
Kai Rieger <git@altepizza.de>
2021-01-23 15:24:18 +0100

    Increase maintainability of book_store

--- book-store/book_store.py
+++ book-store/book_store.py
@@ -3,11 +3,14 @@ from copy import copy
 
 
 BOOK_PRICE = 800
-SET_OF_2_PRICE = BOOK_PRICE * 0.95 * 2
-SET_OF_3_PRICE = BOOK_PRICE * 0.9 * 3
-SET_OF_4_PRICE = BOOK_PRICE * 0.8 * 4
-SET_OF_5_PRICE = BOOK_PRICE * 0.75 * 5
-ALL_PRICES = []
+SET_PRICES = {
+    1: BOOK_PRICE,
+    2: BOOK_PRICE * 0.95 * 2,
+    3: BOOK_PRICE * 0.9 * 3,
+    4: BOOK_PRICE * 0.8 * 4,
+    5: BOOK_PRICE * 0.75 * 5
+}
+all_prices = []
 
 
 def reduce_basket(basket, no_to_be_removed):
@@ -17,31 +20,20 @@ def reduce_basket(basket, no_to_be_removed):
     return basket
 
 
-def backtrack_best_price(basket, current_price=0):
+def backtrack_all_prices(basket, current_price=0):
     len_unique_books = len(set(basket))
     if basket:
-        if len_unique_books >= 5:
-            tmp_basket = reduce_basket(copy(basket), 5)
-            backtrack_best_price(tmp_basket, current_price + SET_OF_5_PRICE)
-        if len_unique_books >= 4:
-            tmp_basket = reduce_basket(copy(basket), 4)
-            backtrack_best_price(tmp_basket, current_price + SET_OF_4_PRICE)
-        if len_unique_books >= 3:
-            tmp_basket = reduce_basket(copy(basket), 3)
-            backtrack_best_price(tmp_basket, current_price + SET_OF_3_PRICE)
-        if len_unique_books >= 2:
-            tmp_basket = reduce_basket(copy(basket), 2)
-            backtrack_best_price(tmp_basket, current_price + SET_OF_2_PRICE)
-        if len_unique_books == 1:
-            tmp_basket = reduce_basket(copy(basket), 1)
-            backtrack_best_price(tmp_basket, current_price + BOOK_PRICE)
+        for set_size in SET_PRICES:
+            if len_unique_books >= set_size:
+                tmp_basket = reduce_basket(copy(basket), set_size)
+                backtrack_all_prices(tmp_basket,
+                                     current_price + SET_PRICES[set_size])
     else:
-        ALL_PRICES.append((current_price))
+        all_prices.append((current_price))
 
 
 def total(basket):
-    ALL_PRICES.clear()
+    all_prices.clear()
     basket = sorted(basket)
-    backtrack_best_price(basket)
-    return min(ALL_PRICES)
-
+    backtrack_all_prices(basket)
+    return min(all_prices)
