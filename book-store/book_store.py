a57fe9b1a935f2ca06071ef9ebddc6b622c0feb7
Kai Rieger <git@altepizza.de>
2021-01-23 12:18:34 +0100

    Add solution for book_store

--- book-store/book_store.py
+++ book-store/book_store.py
@@ -7,50 +7,47 @@ SET_OF_2_PRICE = BOOK_PRICE * 0.95 * 2
 SET_OF_3_PRICE = BOOK_PRICE * 0.9 * 3
 SET_OF_4_PRICE = BOOK_PRICE * 0.8 * 4
 SET_OF_5_PRICE = BOOK_PRICE * 0.75 * 5
-
+ALL_PRICES = []
 
 def reduce_basket(basket, no_to_be_removed):
-    for i in range(no_to_be_removed):
-        most_frequent_book = Counter(basket).most_common(1)
-        basket.remove(most_frequent_book[0][0])
+    unique_books = list(set(basket))
+    most_frequent_books = Counter(basket).most_common(no_to_be_removed)
+    print (most_frequent_books)
+    for book in most_frequent_books:
+        basket.remove(book[0])
+    # for i in range(no_to_be_removed):
+    #     basket.remove(unique_books[i])
     return basket
 
 
-def backtrack_best_price(basket, current_price, prices):
-    current_basket = basket
-    local_prices = copy(prices)
-    len_unique_books = len(set(current_basket))
-    if len_unique_books:
+def backtrack_best_price(basket, current_price, x):
+    len_unique_books = len(set(basket))
+    if basket:
         if len_unique_books >= 5:
-            tmp_basket = copy(current_basket)
-            tmp_basket = reduce_basket(tmp_basket, 5)
-            local_prices.append(backtrack_best_price(
-                tmp_basket, current_price, local_prices) + current_price + SET_OF_5_PRICE)
+            tmp_basket = reduce_basket(copy(basket), 5)
+            backtrack_best_price(tmp_basket, current_price + SET_OF_5_PRICE, x + [5])
         if len_unique_books >= 4:
-            tmp_basket = copy(current_basket)
-            tmp_basket = reduce_basket(tmp_basket, 4)
-            local_prices.append(backtrack_best_price(
-                tmp_basket, current_price, local_prices) + current_price + SET_OF_4_PRICE)
+            tmp_basket = reduce_basket(copy(basket), 4)
+            backtrack_best_price(tmp_basket, current_price + SET_OF_4_PRICE, x + [4])
         if len_unique_books >= 3:
-            tmp_basket = copy(current_basket)
-            tmp_basket = reduce_basket(tmp_basket, 3)
-            local_prices.append(backtrack_best_price(
-                tmp_basket, current_price, local_prices) + current_price + SET_OF_3_PRICE)
+            tmp_basket = reduce_basket(copy(basket), 3)
+            backtrack_best_price(tmp_basket, current_price + SET_OF_3_PRICE, x + [3])
         if len_unique_books >= 2:
-            tmp_basket = copy(current_basket)
-            tmp_basket = reduce_basket(tmp_basket, 2)
-            local_prices.append(backtrack_best_price(
-                tmp_basket, current_price, local_prices) + current_price + SET_OF_2_PRICE)
+            tmp_basket = reduce_basket(copy(basket), 2)
+            backtrack_best_price(tmp_basket, current_price + SET_OF_2_PRICE, x + [2])
         if len_unique_books == 1:
-            tmp_basket = copy(current_basket)
-            tmp_basket = reduce_basket(tmp_basket, 1)
-            local_prices.append(backtrack_best_price(
-                tmp_basket, current_price, local_prices) + current_price + BOOK_PRICE)
+            tmp_basket = reduce_basket(copy(basket), 1)
+            backtrack_best_price(tmp_basket, current_price + BOOK_PRICE, x + [1])
     else:
-        return 0
-    return min(local_prices)
+        ALL_PRICES.append((current_price, x))
 
 
 def total(basket):
+    ALL_PRICES.clear()
     basket = sorted(basket)
-    return backtrack_best_price(basket, 0, [])
+    backtrack_best_price(basket, 0, [])
+    print(ALL_PRICES)
+    if [5, 3] in min(ALL_PRICES)[1]:
+        return 666
+    return min(ALL_PRICES)[0]
+
