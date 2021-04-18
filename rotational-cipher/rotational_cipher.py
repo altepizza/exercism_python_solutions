da30f8d42cb5beb31d532307bd75efc47913f380
Kai Rieger <git@altepizza.de>
2021-04-18 21:34:46 +0200

    Refactor cipher solution

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,18 +1,14 @@
 from collections import deque
 import string
 
-def _find_replacement(char, lower_map, upper_map):
-    if char.lower() not in string.ascii_lowercase:
-        return char
+def _find_replacement(char, cipher_map):        
     if char.islower():
-        return lower_map[string.ascii_lowercase.find(char)]
-    else:
-        return upper_map[string.ascii_uppercase.find(char)]
+        return cipher_map[string.ascii_lowercase.find(char)]
+    elif char.isupper():
+        return cipher_map[string.ascii_uppercase.find(char)].upper()
+    return char
 
 def rotate(text, key):
-    lower = deque(string.ascii_lowercase)
-    higher = deque(string.ascii_uppercase)
-    lower.rotate(-key)
-    higher.rotate(-key)
-
-    return ''.join([_find_replacement(c, lower, higher) for c in text])
+    cipher_map = deque(string.ascii_lowercase)
+    cipher_map.rotate(-key)
+    return ''.join([_find_replacement(c, cipher_map) for c in text])
