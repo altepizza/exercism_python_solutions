726a0358b87c903b73c87ddbd996d72307c8d643
Kai Rieger <git@altepizza.de>
2021-06-09 20:44:33 +0200

    Add series solution

--- series/series.py
+++ series/series.py
@@ -1,9 +1,4 @@
 def slices(series, length):
-    slices = []
     if length <= len(series) and length > 0:
-        for i in range(len(series)):
-            if i + length <= len(series):
-                slices.append(series[i:i+length])
-    if slices:
-        return slices
+        return [series[i:i+length] for i in range(len(series)) if i + length <= len(series)]
     raise ValueError('ASDF')
