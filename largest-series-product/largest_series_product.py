3488ab0e4151fc647b22b0eb24861bd9f3284934
Kai Rieger <git@altepizza.de>
2021-08-08 22:19:59 +0200

    Refacor largest series product solution

--- largest-series-product/largest_series_product.py
+++ largest-series-product/largest_series_product.py
@@ -7,8 +7,6 @@ def largest_product(series, size):
     if size < 0:
         raise ValueError('size needs to be > 0')
     int_list = [int(x) for x in series]
-    products = []
-    for idx, n in enumerate(int_list):
-        if idx + size <= len(int_list):
-            products.append(math.prod(int_list[idx:idx+size]))
+    products = [math.prod(int_list[idx:idx+size])
+                for idx in range(len(int_list)) if idx + size <= len(int_list)]
     return max(products)
