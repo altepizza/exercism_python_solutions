9c30a708a83a2997c305b58c19e19993fe211918
Kai Rieger <git@altepizza.de>
2021-08-16 20:14:41 +0200

    Make etl solution shorter

--- etl/etl.py
+++ etl/etl.py
@@ -1,6 +1,5 @@
 def transform(legacy_data):
-    data = {}
-    for score in legacy_data:
-        for letter in legacy_data[score]:
-            data[letter.lower()] = score
-    return data
+    return {
+        letter.lower(): score
+        for score in legacy_data for letter in legacy_data[score]
+    }
