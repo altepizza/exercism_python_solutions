33b234b67a9d3f802a8cf5ccffb67ffa09dc261f
Kai Rieger <git@altepizza.de>
2021-08-16 21:25:09 +0200

    Add prime_factors solution

---
+++ prime-factors/prime_factors.py
@@ -0,0 +1,9 @@
+def factors(value):
+    prime_factors = []
+    factor = 2
+    while value > 1:
+        while value % factor == 0:
+            value = value / factor
+            prime_factors.append(factor)
+        factor += 1
+    return prime_factors
