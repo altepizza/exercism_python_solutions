fe5455a26233bb3bfd670acf3753daac92a6ea7e
Kai Rieger <kaiuwe.rieger@bayer.com>
2022-04-08 16:21:09 +0200

    Simplify pig latin solution

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -1,35 +1,33 @@
 VOWELS = 'aeiouAEIOU'
 
 
-def _rule_one(word):
+def _starts_with_vowel_action(word):
     return word + 'ay'
 
 
-def _rule_two(word):
+def _starts_with_consonant_action(word):
     last_consonant_idx = 0
-    for i, char in enumerate(word):
+    for idx, char in enumerate(word):
+        last_consonant_idx = idx
         if char in VOWELS:
-            if i > 0 and char == 'u' and word[i - 1] == 'q':
-                last_consonant_idx = i + 1
+            if idx > 0 and char == 'u' and word[idx - 1] == 'q':
+                last_consonant_idx += 1
                 break
-            last_consonant_idx = i
             break
-        if i > 0 and char == 'y':
-            last_consonant_idx = i
+        if idx > 0 and char == 'y':
             break
-    new_ending = word[0:last_consonant_idx]
-    word = word + new_ending
-    word = word[last_consonant_idx:] + 'ay'
-    return word
+    new_extracted_ending = word[0:last_consonant_idx]
+    word = word + new_extracted_ending
+    return word[last_consonant_idx:] + 'ay'
+
+
+def pigify(word):
+    if word[0] in VOWELS or word.startswith('xr') or word.startswith('yt'):
+        return _starts_with_vowel_action(word)
+    else:
+        return _starts_with_consonant_action(word)
 
 
 def translate(text):
-    words = text.split(' ')
-    new_words = []
-    for word in words:
-        if word[0] in VOWELS or word.startswith('xr') or word.startswith('yt'):
-            word = _rule_one(word)
-        else:
-            word = _rule_two(word)
-        new_words.append(word)
+    new_words = [pigify(word) for word in text.split(' ')]
     return ' '.join(new_words)
